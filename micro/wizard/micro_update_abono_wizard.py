adf9e7ab0c734c97ecca327713bec97f0ad9de65
aguevarac <aguevarac@armada.mil.ec>
2023-04-14 10:40:32 -0500

    correcion abono

--- micro/wizard/micro_update_abono_wizard.py
+++ micro/wizard/micro_update_abono_wizard.py
@@ -4,31 +4,28 @@ from odoo import fields, models, api
 
 
 class UpdateAbonoWizard(models.TransientModel):
-    _name = "update.abono.wizard"
+    _name = "micro.update.abono.wizard"
 
     name = fields.Char(string='Código', required=True, readonly=True, copy=False, default='New')
     idFactura = fields.Many2one(
         'micro.ticket', 
         string='Factura', 
-        #default=lambda self: self.env.user.employee_id,domain="[('company_id','=',reparto_id)]" 
+        default=lambda self: self.env['micro.ticket'].search([('id', '=', self._context['active_id'])])
     )
     fch_Movimiento = fields.Datetime(string='Fecha', default=fields.Datetime.now())
     seleccion_tipo = [('creacion', 'Creación'),('abono', 'Abono'),('cancelacion', 'Cancelación')]
-    tipo = fields.Selection(seleccion_tipo, 'Tipo', default='creacion', tracking=True)
+    tipo = fields.Selection(seleccion_tipo, 'Tipo', default='abono', tracking=True, readonly=True)
     currency_id = fields.Many2one(
         comodel_name='res.currency',
         string='Moneda',
         default=lambda self: self.env.company.currency_id.id,
     )
-    valor = fields.Monetary(string='Total')
+    valor = fields.Monetary(string='Valor', default=lambda self: self.env['micro.ticket'].search([('id', '=', self._context['active_id'])]).saldo)
 
 
     _sql_constraints = [
         ('name_unique', 'unique(name)', 'El código debe ser único')
     ]
-
-    # Campo sequence
-    mi_modelo_sequence = fields.Char(string='Sequence', required=True, copy=False, readonly=True, default=lambda self: 'New')
 
     @api.model
     def create(self, vals):
@@ -38,8 +35,8 @@ class UpdateAbonoWizard(models.TransientModel):
         return result
     
     def update_vista_general(self):
-        presupuesto_obj = self.env['micro.ticket']
-        presupuesto_id = presupuesto_obj.search([('id', '=', self._context['active_id'])])
-        presupuesto_id = presupuesto_obj.browse(self._context['active_id'])
-        presupuesto_id.abono += self.valor
-        presupuesto_id.saldo = presupuesto_id.saldo - presupuesto_id.abono
+        ticket_obj = self.env['micro.ticket']
+        ticket_id = ticket_obj.search([('id', '=', self._context['active_id'])])
+        # ticket_id = ticket_obj.browse(self._context['active_id'])
+        ticket_id.abono += self.valor
+        ticket_id.saldo = ticket_id.total - ticket_id.abono
